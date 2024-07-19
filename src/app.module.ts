b4500b403ea1e9223743a29fdc7117452cdafbfe
Tuan Nguyen <ntuan.2502@gmail.com>
2024-07-19 14:09:14 +0700

    install prisma with postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
